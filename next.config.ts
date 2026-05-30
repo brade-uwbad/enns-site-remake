04a8c7df8bb5c5c891a07ef6551626d56b05429b
Raymond2193 <raymond.li2193@gmail.com>
2026-05-30 15:57:15 -0400

    feat: Added url redirects for old websites in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,75 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/about-brad-kitchener-waterloo-real-estate-agent-elmira-new-hamburg",
+        destination: "/about",
+        permanent: true,
+      },
+
+      {
+        source: "/contact-brad-selling-my-home-kitchener-waterloo-ontario",
+        destination: "/contact",
+        permanent: true,
+      },
+
+      {
+        source: "/real-estate-resources-kitchener-waterloo",
+        destination: "/services",
+        permanent: true,
+      },
+
+      {
+        source: "/buy-a-house-kitchener-new-hamburg-elmira-ontario",
+        destination: "/services",
+        permanent: true,
+      },
+
+      {
+        source: "/selling-my-home-waterloo-ontario",
+        destination: "/services",
+        permanent: true,
+      },
+
+      {
+        source: "/reviews",
+        destination: "/about",
+        permanent: true,
+      },
+
+      {
+        source: "/kitchenerwaterloo-real-estate-blog",
+        destination: "/",
+        permanent: true,
+      },
+
+      {
+        source: "/search-kitchener-waterloo-homes-for-sale",
+        destination: "/listings",
+        permanent: true,
+      },
+
+      {
+        source: "/kitchener-waterloo-houses-sale",
+        destination: "/listings",
+        permanent: true,
+      },
+
+      {
+        source: "/kitchener-waterloo-real-estate-agent/:path*",
+        destination: "/listings",
+        permanent: true,
+      },
+
+    ];
+  },
+
+
+
+
+
 };
 
 export default nextConfig;
